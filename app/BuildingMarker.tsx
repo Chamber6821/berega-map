4dd6ac41ec2cf700dd44e1c1697370b350e931da
Chamber6821 <vania.osinin@yandex.ru>
2024-07-21 18:31:39 +0300

    feat: add BuildingMarker

---
+++ app/BuildingMarker.tsx
@@ -0,0 +1,16 @@
+import L, { LatLngExpression } from "leaflet";
+import { Marker } from "react-leaflet";
+
+export default function BuildingMarker({ position, color, children }:
+  { position: LatLngExpression, color: string, children: any }) {
+  return <Marker
+    position={position}
+    icon={L.icon({
+      iconUrl: `https://img.icons8.com/ios-filled/100/${color.replace("#", "")}/100-percents.png`,
+      iconSize: L.point(20, 20),
+      iconAnchor: L.point(10, 10)
+    })}
+  >
+    {children}
+  </Marker>
+}
--- app/Map.tsx
+++ app/Map.tsx
@@ -2,35 +2,28 @@
 
 import L from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import BuildingMarker from "./BuildingMarker";
 
 export default function Map() {
   return <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
     <TileLayer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker
+    <BuildingMarker
       position={[51.505, -0.09]}
-      icon={L.icon({
-        iconUrl: "https://img.icons8.com/ios-filled/100/395296/100-percents.png",
-        iconSize: L.point(20, 20),
-        iconAnchor: L.point(10, 10)
-      })}
+      color="#395296"
     >
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
-    </Marker>
-    <Marker
+    </BuildingMarker>
+    <BuildingMarker
       position={[51.505, 0]}
-      icon={L.icon({
-        iconUrl: "https://img.icons8.com/ios-filled/100/439639/100-percents.png",
-        iconSize: L.point(20, 20),
-        iconAnchor: L.point(10, 10)
-      })}
+      color="#439639"
     >
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
-    </Marker>
+    </BuildingMarker>
   </MapContainer>
 }
