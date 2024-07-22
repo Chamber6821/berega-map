1b037a833b62617e4ccfa874fdd44148f52ed173
Chamber6821 <vania.osinin@yandex.ru>
2024-07-22 14:15:12 +0300

    feat: add default clusterer

--- app/map/Map.tsx
+++ app/map/Map.tsx
@@ -2,12 +2,15 @@
 
 import { LatLngExpression } from "leaflet";
 import { MapContainer, TileLayer } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 export default function Map({ center, zoom, children }: { center: LatLngExpression, zoom: number, children: any }) {
   return <MapContainer center={center} zoom={zoom} scrollWheelZoom={true}>
     <TileLayer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    {children}
+    <MarkerClusterGroup>
+      {children}
+    </MarkerClusterGroup>
   </MapContainer>
 }
