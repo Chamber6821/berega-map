93043cfc7acd037c0a65f1114ea97c7f258fed99
Chamber6821 <vania.osinin@yandex.ru>
2024-07-22 18:55:44 +0300

    refactor: move cards from Content into Cards

---
+++ app/Cards.tsx
@@ -0,0 +1,18 @@
+import { Building } from "./api/berega";
+import Card from "./Card";
+
+export default function Cards({ buildings }: { buildings: Building[] }) {
+  return <div className="cards-container">
+    {
+      buildings
+        .map(
+          x => <Card
+            key={x.page}
+            image={x.image}
+            title={x.title}
+            description={x.shortDescription}
+            page={x.page}
+          />)
+    }
+  </div>
+}
--- app/Content.tsx
+++ app/Content.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Building } from "./api/berega";
-import Card from "./Card";
+import Cards from "./Cards";
 import { BuildingMarker, Map } from "./map";
 import { average } from "./utils";
 
@@ -17,25 +17,7 @@ export default function Content({ buildings }:
           .map(x => <BuildingMarker key={x.page} position={[x.lat, x.lng]} color={x.color} />)
       }
     </Map>
-    <div className="cards-container">
-      {
-        buildings
-          .map(
-            x => <Card
-              key={x.page}
-              image={x.image}
-              title={x.title}
-              description={x.shortDescription}
-              page={x.page}
-            />)
-      }
-      <Card
-        image="https://7e84f1a57df011c41fb576b3421bc0e8.cdn.bubble.io/f1700519301609x437900309108657100/3335.jpg"
-        title="Blue Sky Tower"
-        description={["174 апартаментов", "от $ 46 150"]}
-        page=""
-      />
-    </div>
+    <Cards buildings={buildings} />
   </div >
 
 }
