029907f6352cba522d9649523f4bf33ed3039d3b
Chamber6821 <vania.osinin@yandex.ru>
2024-07-22 16:43:58 +0300

    feat: custom marker of clusters

--- app/map/Map.tsx
+++ app/map/Map.tsx
@@ -1,15 +1,24 @@
 'use client'
 
-import { LatLngExpression } from "leaflet";
+import L, { LatLngExpression } from "leaflet";
 import { MapContainer, TileLayer } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
+
+const iconForCluster = (cluster: any) => L.divIcon({
+  html: `<p>${cluster.getChildCount()}</p>`,
+  className: 'cluster-icon',
+  iconSize: L.point(30, 30)
+})
 
 export default function Map({ center, zoom, children }: { center: LatLngExpression, zoom: number, children: any }) {
   return <MapContainer center={center} zoom={zoom} scrollWheelZoom={true}>
     <TileLayer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <MarkerClusterGroup>
+    <MarkerClusterGroup
+      iconCreateFunction={iconForCluster}
+      maxClusterRadius={40}
+    >
       {children}
     </MarkerClusterGroup>
   </MapContainer>
