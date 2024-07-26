36b77ed8f27f2a420f42f7785501d21f914cc283
Chamber6821 <vania.osinin@yandex.ru>
2024-07-26 19:28:33 +0300

    feat: remove marker clusterer

--- app/map/Map.tsx
+++ app/map/Map.tsx
@@ -2,18 +2,11 @@
 
 import L, { LatLngBounds, LatLngExpression } from "leaflet";
 import { MapContainer, TileLayer } from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-cluster";
 import SelectionArea from "./SelectionArea";
 import { useState } from "react";
 import Control from "react-leaflet-custom-control";
 import "leaflet/dist/leaflet.css"
 import ViewBounds from "./ViewBounds";
-
-const iconForCluster = (cluster: any) => L.divIcon({
-  html: `<p>${cluster.getChildCount()}</p>`,
-  className: 'cluster-icon',
-  iconSize: L.point(30, 30)
-})
 
 export type Bounds = L.LatLngBounds
 
@@ -28,12 +21,7 @@ export default function Map({ center, zoom, onBoundsChanged, children }:
     <TileLayer
       url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=uL0rwqFI3ByfHOEQrTJP"
     />
-    <MarkerClusterGroup
-      iconCreateFunction={iconForCluster}
-      maxClusterRadius={40}
-    >
-      {children}
-    </MarkerClusterGroup>
+    {children}
     <SelectionArea drawing={drawMode} />
     <ViewBounds onChanged={onBoundsChanged} />
     <Control prepend position="topright">
