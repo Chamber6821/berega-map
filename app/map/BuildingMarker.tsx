ee68f7a3761c9d6a91f1b3428ff9e1e3cd22c91e
Chamber6821 <vania.osinin@yandex.ru>
2024-07-26 19:52:37 +0300

    feat: change marker size

--- app/map/BuildingMarker.tsx
+++ app/map/BuildingMarker.tsx
@@ -5,12 +5,13 @@ import { Marker } from "react-leaflet";
 
 export default function BuildingMarker({ position, color, children = undefined, onClick }:
   { position: LatLngExpression, color: string, children?: any, onClick?: LeafletMouseEventHandlerFn }) {
+  const size = 12
   return <Marker
     position={position}
     icon={L.icon({
       iconUrl: `https://img.icons8.com/ios-filled/100/${color.replace("#", "")}/100-percents.png`,
-      iconSize: L.point(14, 14),
-      iconAnchor: L.point(7, 7),
+      iconSize: L.point(size, size),
+      iconAnchor: L.point(size / 2, size / 2),
       className: 'marker',
     })}
     eventHandlers={{
