2b3c943972f75da535a1e6d9026058bcf9806873
Chamber6821 <vania.osinin@yandex.ru>
2024-08-05 17:01:28 +0300

    feat(Modal): close on click by overlay

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -2,13 +2,27 @@ import { CloseOutline } from "react-ionicons";
 import CloseButton from "./CloseButton";
 import Overlay from "./Overlay";
 import Paper from "./Paper";
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect, useRef } from "react";
+
+const useOnClickOutside = (ref: { current: HTMLElement | null }, handler: () => void) => {
+  useEffect(() => {
+    const onClick = (e: MouseEvent) => {
+      const container = ref.current
+      if (container && !container.contains(e.target as HTMLElement))
+        handler()
+    }
+    document.addEventListener('click', onClick, true)
+    return () => document.removeEventListener('click', onClick, true)
+  }, [ref, handler])
+}
 
 export default function Modal({ style, paperStyle, onClose, children }:
   { style?: CSSProperties, paperStyle?: CSSProperties, onClose?: () => void, children?: any }) {
+  const ref = useRef<HTMLDivElement>(null)
+  useOnClickOutside(ref, onClose || (() => { }))
   return (
     <Overlay style={style}>
-      <Paper style={paperStyle}>
+      <Paper ref={ref} style={paperStyle}>
         <CloseButton onClick={onClose}>
           <CloseOutline color={"#050b0d"} />
         </CloseButton>
