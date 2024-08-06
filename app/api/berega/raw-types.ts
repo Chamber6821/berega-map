a7145ef2ad6e124d22ace10f3ed7ac009518d2c0
Chamber6821 <vania.osinin@yandex.ru>
2024-08-06 17:40:58 +0300

    feat(raw-types): add RawSecondHome validator

--- app/api/berega/raw-types.ts
+++ app/api/berega/raw-types.ts
@@ -1,3 +1,7 @@
+export const SecondHomeRoomVariants = ['Studio', '1+1', '2+1', '3+1', '4+1', '5+'] as const
+export const SecondHomeStatusVariants = ['Новостройки', 'Вторичное жилье'] as const
+export const SecondHomeTypeVariants = ['Дом', 'Квартира', 'Земельный участок', 'Коммерческая недвижимость'] as const
+
 export type RawSecondHome = {
   address: {
     address: string,
@@ -12,10 +16,10 @@ export type RawSecondHome = {
   pictures: string[],
   price: number,
   price_per_meter: number,
-  rooms: 'Studio' | '1+1' | '2+1' | '3+1' | '4+1' | '5+',
-  'status (OS)': 'Новостройки' | 'Вторичное жилье',
+  rooms: typeof SecondHomeRoomVariants[number],
+  'status (OS)': typeof SecondHomeStatusVariants[number],
   total_area: number,
-  Type: 'Дом' | 'Квартира' | 'Земельный участок' | 'Коммерческая недвижимость',
+  Type: typeof SecondHomeTypeVariants[number],
   'Created Date': string,
 }
 
@@ -42,3 +46,37 @@ export type RawApartment = {
   status: string,
   total_area: number,
 }
+
+export const isStringProperty = <Key extends string>(x: any, key: Key): x is { [key in Key]: string } =>
+  key in x && typeof x[key] === 'string'
+
+export const isNumberProperty = <Key extends string>(x: any, key: Key): x is { [key in Key]: number } =>
+  key in x && typeof x[key] === 'number'
+
+export const isObjectProperty = <Key extends string>(x: any, key: Key): x is { [key in Key]: object } =>
+  key in x && typeof x[key] === 'object'
+
+export const isArrayProperty = <Key extends string>(x: any, key: Key): x is { [key in Key]: Array<any> } =>
+  key in x && typeof x[key] === 'object' && 'length' in x[key]
+
+export const isAddress = (x: object): x is { address: string, lng: number, lat: number } =>
+  isStringProperty(x, 'address')
+  && isNumberProperty(x, 'lat')
+  && isNumberProperty(x, 'lng')
+
+export const isRawSecondHome = (x: object): x is RawSecondHome =>
+  isObjectProperty(x, 'address') && x.address !== null && isAddress(x.address)
+  && isStringProperty(x, 'city (OS)')
+  && isStringProperty(x, 'country (OS)')
+  && isNumberProperty(x, 'floor')
+  && isNumberProperty(x, 'frame (OS)')
+  && isStringProperty(x, 'name')
+  && isArrayProperty(x, 'pictures') && x.pictures.every(x => typeof x === 'string')
+  && isNumberProperty(x, 'price')
+  && isNumberProperty(x, 'price_per_meter')
+  && isStringProperty(x, 'rooms') && SecondHomeRoomVariants.includes(x.rooms as any)
+  && isStringProperty(x, 'status (OS)') && SecondHomeStatusVariants.includes(x['status (OS)'] as any)
+  && isNumberProperty(x, 'total_area')
+  && isStringProperty(x, 'Type') && SecondHomeTypeVariants.includes(x.Type as any)
+  && isStringProperty(x, 'Created Date')
+
