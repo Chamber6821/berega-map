0dfc7c3dcdbe1154f709cdc475b9bd17cd93141b
Chamber6821 <vania.osinin@yandex.ru>
2024-09-08 10:12:12 +0300

    split companion and user messages

--- app/components/chat/ChatBot.tsx
+++ app/components/chat/ChatBot.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import Chat from "./Chat";
 import MyMessage from "./MyMessage";
+import CompanionMessage from "./CompanionMessage";
+
+type Message = {
+  author: 'user' | 'bot',
+  text: string
+}
 
 export default function ChatBot({ onClose }: { onClose?: () => void }) {
-  const [history, setHistory] = useState<string[]>([])
-  return <Chat onClose={onClose} onSend={x => setHistory([...history, x])}>
+  const [history, setHistory] = useState<Message[]>([])
+  return <Chat onClose={onClose} onSend={x => setHistory([...history, { author: 'user', text: x }])}>
     {
-      history.map(x => <MyMessage key={x}>{x}</MyMessage>)
+      history.map(x =>
+        x.author === 'user'
+          ? <MyMessage key={x.author + x.text}>{x.text}</MyMessage>
+          : <CompanionMessage key={x.author + x.text}>{x.text}</CompanionMessage>
+      )
     }
-  </Chat >
+  </Chat>
 }
