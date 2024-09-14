c6aed5de4ef133fdca4c3792dba45e7248f1c718
Chamber6821 <vania.osinin@yandex.ru>
2024-09-14 15:27:14 +0300

    Refactor useClusters

--- app/storages/useClusters.ts
+++ app/storages/useClusters.ts
@@ -1,19 +1,18 @@
-import {fetchPointsCounter, PointsCountTypeOpenApi} from "../api/openApi"
+import { fetchPointsCounter, PointsCountTypeOpenApi } from "../api/openApi"
 import { Filters } from "../filters/useFilters"
 import { create } from "zustand";
 
 export type ClustersStorage = {
   clusters: PointsCountTypeOpenApi[],
-  load: (filters: Filters) => void
+  updateFor: (filters: Filters) => void
 }
 
-export const useClusters = create<ClustersStorage>((set, get) => {
+export const useClusters = create<ClustersStorage>(set => {
   return {
     clusters: [],
-    firstLoad: (filters: Filters) => {
-      set(async (state) => ({
-        clusters: await fetchPointsCounter(filters)
-      }));
+    updateFor: async (filters: Filters) => {
+      const clusters = await fetchPointsCounter(filters)
+      set({ clusters })
     }
   }
-});
+})
