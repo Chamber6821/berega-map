e7f9001bb006b19a3fcded8c708166d0dcfa9852
Chamber6821 <vania.osinin@yandex.ru>
2024-09-14 15:30:28 +0300

    Implement usePoints

--- app/storages/usePoints.ts
+++ app/storages/usePoints.ts
@@ -1,22 +1,17 @@
-import {fetchFilteredPoints, PointsCountTypeOpenApi, PointsTypeOpenApi} from "../api/openApi"
+import { fetchFilteredPoints, PointsCountTypeOpenApi, PointsTypeOpenApi } from "../api/openApi"
 import { Filters } from "../filters/useFilters"
 import { create } from "zustand"
 
 export type PointsStorage = {
   points: PointsTypeOpenApi[],
-  filters: Filters,
-  load: (filters: Filters, mapCenter: [number, number]) => void
+  updateFor: (filters: Filters, mapCenter: [number, number]) => void
 }
 
-export const usePoints = create<PointsStorage>((set, get) => {
-  return {
-    points: [],
-    filters: {},
-    firstLoad: (filters: Filters, mapCenter: [number, number]) => {
-      set(async (state) => ({
-        points: await fetchFilteredPoints(mapCenter, 100000, filters)
-      }));
-    }
+export const usePoints = create<PointsStorage>(set => ({
+  points: [],
+  updateFor: async (filters: Filters, mapCenter: [number, number]) => {
+    const points = await fetchFilteredPoints(mapCenter, 100000, filters)
+    set({ points })
   }
-});
+}))
 
