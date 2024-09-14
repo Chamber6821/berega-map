89d3c76d5224e3ccc4e01d67aea0ee0aac9555d4
Chamber6821 <vania.osinin@yandex.ru>
2024-09-14 15:24:35 +0300

    Implement useBuildingMap

--- app/storages/useBuildingMap.ts
+++ app/storages/useBuildingMap.ts
@@ -1,21 +1,25 @@
 import { Building } from "../api/berega";
-import { PointsTypeOpenApi } from "../api/openApi";
+import { fetchBuilding, PointsTypeOpenApi } from "../api/openApi";
 import { create } from 'zustand';
 
-export type BuildignMapStorage = {
-  map: { [key: number]: Building },
+export type BuildingMapStorage = {
+  map: { [key: string]: Building },
   forPoint: (point: PointsTypeOpenApi) => Building | undefined,
   loadForPoint: (point: PointsTypeOpenApi) => void,
 }
 
-export const useBuildingMap = create<BuildignMapStorage>((set, get) => {
+export const useBuildingMap = create<BuildingMapStorage>((set, get) => {
   return {
-    map: ,
-    forPoint: async (point: PointsTypeOpenApi) => {
-
-    },
-    loadForPoint: (point: PointsTypeOpenApi) => {
-
+    map: {},
+    forPoint: (point: PointsTypeOpenApi) => get().map[point.id],
+    loadForPoint: async (point: PointsTypeOpenApi) => {
+      const building = await fetchBuilding(point.id)
+      set(x => ({
+        map: {
+          ...x.map,
+          [point.id]: building
+        }
+      }))
     }
   }
 });
